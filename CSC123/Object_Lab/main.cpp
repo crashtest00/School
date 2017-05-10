45cd1892304ffedb0ef70449e23ab44987966d75
crashtest84 <jjmarck@gmail.com>
2017-05-10 00:35:39 -0500

    Object Lab update
    Less psuedo code, much more of the real thing. Remaining tasks:
    - Missing many functions from the header file
    - Header file needs to be it's own thing
    - Header file should probably be renamed (cardgame)
    - binaryYN needs added to main()

--- CSC123/Object_Lab/main.cpp
+++ CSC123/Object_Lab/main.cpp
@@ -1,52 +1,104 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <stdlib.h>
+#include <iomanip>
 
 using namespace std;
 
-    Create a card class
-        Card class should include an accessor that translates
-        {jack queen king} as well as suit:
-            int main()
-        Add a show card function that outputs strings instead of int
-{
-  cout << "\003" << endl;
-  cout << "\004" << endl;
-  cout << "\005" << endl;
-  cout << "\006" << endl;
-  return 0;
-}
-        Maybe a switch for this instead of else if?
+//Should accessor functions be void?
+//Where does the pretty card function go?
+//This is sort of in the middle of using a class for Hand and using a vector in main() for Hand
 
-    create a hand subclass
-        All of these should be private functions
-        Add a sort function
-            Sort by rank
-        Add a show hand function
-            Iterate through hand array using the showCard method
-        Score the hand
-        Look at the dice game for how to score
-            Remember, the hand has been sorted. Which means a straight has multiple rank in a row
-                if nextCard == (currentCard - 1), increment straight
-                if nextCard === (currentCard) increment ofAKind
-            Of a kind means multiple
-                Need to provide nice clean results with proper plurals and 3 of a kind. Go verbose.
+class Card
+{
+    //string cardID();
+public:
+    Card();
+    Card(int rankInt, int suitInt);
+
+    //Mutator
+    void add(int, int);
+
+    //Accessor
+    string getCard(); //This is simply getRank + getSuit
+    string getRank(); //Needs to translate numbers into names when req. Maybe a switch for this instead of else if?
+    string getSuit();
+
+private:
+    int rankInt;
+    int suitInt;
+};
+
+class Hand : public Card
+{
+public:
+    Hand();
+    Hand(vector<Card> cards);
+
+    //Mutator
+    void addCard(Card);
+//        cards.push_back(Card) //How to write this function?
+
+
+    //Accessor
+    string showHand(); //Loop through vector using the getCard function
+    string getScore(); //Should this call a private rules function?
+    void sortCards(); // sort(myHand.begin(), hand.end(), compareCards()); //
+    //Rules
+//            if nextCard == (currentCard - 1), increment straight
+//                if nextCard === (currentCard) increment ofAKind
+//            Of a kind means multiple
+//                Need to provide nice clean results with proper plurals and 3 of a kind. Go verbose.
+
+private:
+    vector<Card> cards;
+    void sort(vector<Card>); //Iterate through hand array using Card.rankInt
+};
+void buildDeck(vector<Card>&); //This is in main() right now
+void shuffleDeck(vector<Card>&); //Needs written
+//void? binaryYN
 
 int main()
 {
-    need two loops to create the deck
-       From 1-4 i
-        Add a card from 1-14 j
-        Load the deck into a vector<cards>
-    Display the deck in 4 neat columns (reference array lab for this)
-        Should call the
-    system(break);
-    system(cls);
-    Shuffle the vector with random_shuffle
-    "Deal" 5 cards into the hand class
-    display the unsorted hand
-    sort the hand
-    display the sorted hand
+    vector<Card> deck;
+    int sizeOfHand = 5; //5 cards in each hand
+    //vector<Card> hand;
+    Hand myHand;
+
+    buildDeck(deck); //Build the deck
+    shuffleDeck(deck); //Shuffle the deck
+
+    for(int i=0; i<sizeOfHand;i++) //Deal the hand
+        myHand.addCard(deck[i]);
+
+    myHand.showHand(); //Unsorted hand
+    myHand.sortCards();
+    myHand.showHand(); //Sorted hand
+
+    cout << myHand.getScore(); //Show result of hand
+
+    cout << "Play again?";
+    binaryYN; //Need to add this function
 }
 
-Ace can be high or low
-    Only matters for straights and the numeric assignment
+void buildDeck(vector<Card> &myDeck){
+    Card tempCard;
+
+    cout << "Column headers go here"; //column headers (suits)
+    for(int i=0; i< 14; i++){ //loop through ranks
+        for (int j=0; j<4; j++){ //loop through suits
+            tempCard.add(i,j);
+            myDeck.push_back(tempCard);
+            cout << myDeck.back().getRank(); //print ranks to screen
+            cout << setw(10); //Need to check column spacing, reference array lab for this
+        }
+        cout << endl;
+    }
+    system("pause");
+    system("cls");
+}
+
+void shuffleDeck(vector<Card> &myDeck){
+    cout << "This will shuffle the deck"; //Use random_shuffle, this function needs written
+}
